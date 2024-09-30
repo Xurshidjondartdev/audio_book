ee97dcbf942445f78ceb6144f4d90bd9ad0673fe
Xurshidjon Umarov <thebestxurshidjon@example.com>
2024-09-30 14:21:10 +0500

    add package

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:audio_book/core/manager/setting_contoller.dart';
 import 'package:audio_book/features/main/main_logic_impl.dart';
 import 'package:audio_book/my_app.dart';
+import 'package:audio_book/setup.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
+  await setup();
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   Get.put(SettingsController(), permanent: true);
